b3aab9065d99ef6cfb797d1120196d935574b330
Stephen Ayotte <stephen.ayotte@salesforce.com>
2014-09-03 10:17:40 -0400

    startup ordering

--- src/main/java/akkamud/AkkaMud.java
+++ src/main/java/akkamud/AkkaMud.java
@@ -21,18 +21,16 @@ public class AkkaMud
     {
         final ActorSystem system = ActorSystem.create("mud-actorsystem");
 
+        System.out.println("AkkaMUD: loading rooms");
         final ActorRef purgatory = system.actorOf(Props.create(Purgatory.class), "purgatory");
         final ActorRef roomSup = system.actorOf(Props.create(RoomSupervisor.class),
         										"room-supervisor");
         long startTime = System.nanoTime();
-        Future<Object> f = Patterns.ask(roomSup,  new LoadRooms(), 2000);
-        Await.ready(f, Duration.create(2000, "millis"));
+        Future<Object> f = Patterns.ask(roomSup,  new LoadRooms(), 100);
+        Await.ready(f, Duration.create(5, "minutes"));
         long endTime = System.nanoTime();
         long durationMS = (endTime - startTime) / 1000000;
         System.out.println("AkkaMUD: completed room load in " + durationMS + "ms");
-
-        final ActorRef mobileSup = system.actorOf(Props.create(MobileSupervisor.class),
-                                                  "mobile-supervisor");
 
         startTime = System.nanoTime();
     	ActorRef room1 = null; 
@@ -57,9 +55,12 @@ public class AkkaMud
     			System.out.println("Caught exception resolving room1, trying again forever...: " + e);
     		}
     	}
+    	
+        final ActorRef mobileSup = system.actorOf(Props.create(MobileSupervisor.class),
+                                                  "mobile-supervisor");
     	mobileSup.tell(new SetDefaultRoom(room1), null);
-        f = Patterns.ask(mobileSup, new StartChildren(),  1000);
-        Await.ready(f, Duration.create(1000, "millis"));
+        f = Patterns.ask(mobileSup, new StartChildren(),  100);
+        Await.ready(f, Duration.create(5, "minutes"));
 //        room1.tell(new Announce(null), room2);
 //        f = Patterns.ask(room1, new Announce(null), 100);
 //        Await.ready(f,  Duration.create(100, "millis"));
